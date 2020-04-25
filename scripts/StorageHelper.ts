d5e91b84fb5941ff4800bcf87949ee81b7b59e6d
avi hadad <avih75@gmail.com>
2020-04-25 13:17:52 +0300

    M

--- scripts/StorageHelper.ts
+++ scripts/StorageHelper.ts
@@ -37,6 +37,13 @@ export async function StoreValue(key: string, value: DocRow) {
     deferred.resolve();
     return deferred;
 }
+export async function StoreValueList(key: string, value: any) {
+    var deferred = $.Deferred();
+    let dataService: any = await VSS.getService(VSS.ServiceIds.ExtensionData);
+    let result = await dataService.setValue(key, value);
+    deferred.resolve();
+    return deferred;
+}
 // export function RetriveValue(key: string): JQueryPromise<any> {
 //     var deferred = $.Deferred();
 //     VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: any) => {
@@ -54,5 +61,42 @@ export async function RetriveValue(key: string) {
     let result: DocRow = undefined;
     let dataService: any = await VSS.getService(VSS.ServiceIds.ExtensionData);
     result = await dataService.getValue(key);
+    // StoreListToStorage("Picker1") to add a doc to storeg
     return result;
 }
+function StoreListToStorage(controlName: string) {
+    // Demo list
+    let doc = {
+        FieldsLists: new Array<Array<FieldValues>>()
+    }
+    let values1: Array<FieldValues> = new Array<FieldValues>();
+    values1.push({ Depend: "", Value: "Hadad" });
+    values1.push({ Depend: "", Value: "Sheler" });
+    doc.FieldsLists.push(values1);
+    let values2: Array<FieldValues> = new Array<FieldValues>();
+    values2.push({ Depend: "Hadad", Value: "Avi" });
+    values2.push({ Depend: "Hadad", Value: "Riki" });
+    values2.push({ Depend: "Sheler", Value: "Dana" });
+    values2.push({ Depend: "Sheler", Value: "Moshe" });
+    doc.FieldsLists.push(values2);
+    let values3: Array<FieldValues> = new Array<FieldValues>();
+    values3.push({ Depend: "HadadAvi", Value: "Pans" });
+    values3.push({ Depend: "HadadRiki", Value: "Dress" });
+    values3.push({ Depend: "ShelerDana", Value: "Shirt" });
+    values3.push({ Depend: "ShelerMoshe", Value: "Pans" });
+    doc.FieldsLists.push(values3);
+    let values4: Array<FieldValues> = new Array<FieldValues>();
+    values4.push({ Depend: "HadadAviPans", Value: "Long" });
+    values4.push({ Depend: "HadadRikiDress", Value: "Red" });
+    values4.push({ Depend: "HadadRikiDress", Value: "Blue" });
+    values4.push({ Depend: "ShelerDanaShirt", Value: "T-Shirt" });
+    values4.push({ Depend: "ShelerMoshePans", Value: "Short" });
+    values4.push({ Depend: "ShelerMoshePans", Value: "Bath" });
+    doc.FieldsLists.push(values4);
+    StoreValueList(controlName, doc);
+    // return RetriveValue(controlName);
+    // RetriveValue(controlName).then((doc2) => {
+    //     let x = doc2
+    // });
+}
+
